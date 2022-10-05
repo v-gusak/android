f2bab1e1738e535794d6d469c00fc403a0de04e0
v-gusak <KamekazeShow@gmail.com>
2022-10-05 17:33:32 +0300

    Fix (for, forEach) example

--- app/src/main/java/lecture2/task.kt
+++ app/src/main/java/lecture2/task.kt
@@ -1,41 +1,30 @@
 package lecture2
 
-fun arrayExample() {
-    //forEach
-    val array = IntArray(5) { it * 1 }
-    array.forEach { println(it) }
-
-    print("\n")
-
-    //for
-    for (i in 0..4) {
-        println(array[i])
-    }
-}
-
 fun main() {
     val dog = Dog(5, true, 15)
     dog.run()
-    dog.aliveOrNot()
-    dog.showAge()
     dog.makeQuietNoise()
     dog.makeLoudNoise()
     dog.whatIsEating()
     print("Tail length: ${dog.getTailLength()}\n\n")
 
     val duck = Duck(1, true, 4)
-    duck.aliveOrNot()
-    duck.showAge()
     duck.makeQuietNoise()
     duck.makeLoudNoise()
     duck.whatIsEating()
     print("Beak length: ${duck.getBeakLength()}\n\n")
 
-    val fish = Fish(2, true, "silver")
-    fish.aliveOrNot()
-    fish.showAge()
+    val fish = Fish(2, false, "silver")
     fish.whatIsEating()
-    println("Fish color: ${fish.getColor()}\n")
+    println("Color: ${fish.getColor()}\n")
 
-    arrayExample()
+    val animals = listOf(dog, duck, fish)
+
+    animals.forEach { it.showAge() }
+
+    print("\n")
+
+    for (i in animals.indices) {
+        animals[i].aliveOrNot()
+    }
 }
