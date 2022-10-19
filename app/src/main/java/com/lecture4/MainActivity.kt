25a48e4f56f58ae0c3743bb809cfa5e4133454f6
v-gusak <kamekazeshow@gmail.com>
2022-10-19 20:52:40 +0300

    Lecture 4 task

---
+++ app/src/main/java/com/lecture4/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lecture4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+//        setContentView(R.layout.activity_second)
+//        setContentView(R.layout.activity_third)
+    }
+}
